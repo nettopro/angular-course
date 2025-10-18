18efa0f52f21848845388f64322e7996107e8db5
Netto Pro <enzomnf_pro@outlook.com>
2025-10-18 14:04:24 -0300

    studied ng Functions and destroyRef

--- cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,39 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  //Limits the variable to have literal types
+  /* private interval?: ReturnType<typeof setInterval>; */
+  private destroyRef = inject(DestroyRef);
+
+  constructor(){}
+
+  ngOnInit() {
+    console.log('On Init');
+    const interval = setInterval(() => {
+      const rnd = Math.random();
+
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    });
+  }
+
+  ngAfterViewInit() {
+    console.log('After View Init');
+  }
+
+  /* ngOnDestroy() {
+      clearTimeout(this.interval);
+  } */
+
 }
