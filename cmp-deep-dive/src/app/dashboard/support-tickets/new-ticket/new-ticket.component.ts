5d138d4024dd604860586855aa21c78980ea9ad8
Netto Pro <enzomnf_pro@outlook.com>
2025-10-21 13:43:28 -0300

    studied content and view child

--- cmp-deep-dive/src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
+++ cmp-deep-dive/src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ArrowButtonComponent } from "../../../shared/arrow-button/arrow-button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ArrowButtonComponent, ControlComponent],
+  imports: [ArrowButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
-export class NewTicketComponent {
+export class NewTicketComponent implements AfterViewInit{
+  //@ViewChild('form') form ?: ElementRef<HTMLFormElement>;
 
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+  ngAfterViewInit(): void {
+      console.log('After View Init');
+      console.log(this.form().nativeElement);
+      
+      
+  }
+
+  /* onSubmit(titleElement: HTMLInputElement) {
+    const enteredTitle = titleElement.value;
+    console.log(enteredTitle);
+  } */ //One way of using template variables
+
+  onSubmit(title: string, ticketText: string){
+    console.log(title);
+    console.log(ticketText);
+    this.form()?.nativeElement.reset();
+  }
 }
--- cmp-deep-dive/src/app/shared/control/control.component.ts
+++ cmp-deep-dive/src/app/shared/control/control.component.ts
@@ -1,5 +1,8 @@
 import {
+  AfterContentInit,
   Component,
+  contentChild,
+  ContentChild,
   ElementRef,
   HostBinding,
   inject,
@@ -17,20 +20,34 @@ import {
   //The textarea and input styles of the css were not being applied
   //So by removing the encapsulation, the styles were then applied globally
   //And it then worked in this scenario
-   host: {
-     class: 'control',
-     '(click)': 'onClick()'
+  host: {
+    class: 'control',
+    '(click)': 'onClick()',
   },
   //Can also allow for listening to events on host property
 })
-export class ControlComponent {
+export class ControlComponent implements AfterContentInit{
   //@HostBinding('class') className = 'control';
   //Achieves the same as host property, disencouraged as it's obsolete
+
   label = input.required<string>();
   private el = inject(ElementRef);
+  /* @ContentChild('input') private control?: ElementRef<
+    HTMLInputElement | HTMLTextAreaElement
+  >; */
+
+  private control =
+    contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input');
+
+  ngAfterContentInit(): void {
+    console.log('After Content Init');
+    
+  }
+
   //Demonstration of getting information directly of the host element
   onClick() {
     console.log('Clicked!');
     console.log(this.el);
+    console.log(this.control);
   }
 }
