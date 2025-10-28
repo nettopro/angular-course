6ac469243878562afeb72db8c7aebae4c2f76420
Netto Pro <enzomnf_pro@outlook.com>
2025-10-28 13:08:36 -0300

    added delete favorite place

--- http-integration-backend/src/app/places/user-places/user-places.component.ts
+++ http-integration-backend/src/app/places/user-places/user-places.component.ts
@@ -2,9 +2,8 @@ import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
-import { HttpClient } from '@angular/common/http';
-import { Place } from '../place.model';
 import { PlacesService } from '../places.service';
+import { Place } from '../place.model';
 
 @Component({
   selector: 'app-user-places',
@@ -13,7 +12,7 @@ import { PlacesService } from '../places.service';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent implements OnInit{
+export class UserPlacesComponent implements OnInit {
   isFetching = signal(false);
   error = signal('');
   private placesService = inject(PlacesService);
@@ -21,19 +20,30 @@ export class UserPlacesComponent implements OnInit{
   places = this.placesService.loadedUserPlaces;
 
   ngOnInit() {
-      this.isFetching.set(true);
-      const subscription = this.placesService.loadUserPlaces()
-        .subscribe({
-          error: (error: Error) => {
-            this.error.set(error.message);
-          },
-          complete: () => {
-            this.isFetching.set(false);
-          },
-        });
-  
-      this.destroyRef.onDestroy(() => {
-        subscription.unsubscribe();
+    this.isFetching.set(true);
+    const subscription = this.placesService.loadUserPlaces().subscribe({
+      error: (error: Error) => {
+        this.error.set(error.message);
+      },
+      complete: () => {
+        this.isFetching.set(false);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
+  onSelectFavoritePlace(selectedPlace: Place) {
+    const subscription = this.placesService
+      .removeUserPlace(selectedPlace)
+      .subscribe({
+        next: (resData) => console.log(resData),
       });
-    }
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
