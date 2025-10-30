50aa7bf8398ea254f2d75945e7291fb4a51df8d4
Netto Pro <enzomnf_pro@outlook.com>
2025-10-29 23:59:21 -0300

    using nested form groups and form arrays

--- reactive-forms/src/app/auth/signup/signup.component.ts
+++ reactive-forms/src/app/auth/signup/signup.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -11,19 +17,40 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 export class SignupComponent {
   form = new FormGroup({
     email: new FormControl('', {
-      validators: [Validators.email, Validators.required]
+      validators: [Validators.email, Validators.required],
     }),
-    password: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(6)]
+    passwords: new FormGroup({
+      password: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(6)],
+      }),
+      confirmPassword: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(6)],
+      }),
     }),
+    firstName: new FormControl('', { validators: [Validators.required] }),
+    lastName: new FormControl('', { validators: [Validators.required] }),
+    address: new FormGroup({
+      street: new FormControl('', { validators: [Validators.required] }),
+      number: new FormControl('', { validators: [Validators.required] }),
+      postalCode: new FormControl('', { validators: [Validators.required] }),
+      city: new FormControl('', { validators: [Validators.required] }),
+    }),
+    role: new FormControl<
+      'student' | 'teacher' | 'employee' | 'founder' | 'other'
+    >('student', { validators: [Validators.required] }),
+    source: new FormArray([
+      new FormControl(false),
+      new FormControl(false),
+      new FormControl(false),
+    ]),
+    agree: new FormControl(false, { validators: [Validators.required] }),
   });
 
-  onSubmit(){
+  onSubmit() {
     console.log(this.form);
-    
   }
 
-  onReset(){
+  onReset() {
     this.form.reset();
   }
 }
